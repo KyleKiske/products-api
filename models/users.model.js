78f09c38494433ac446892d0255e2cfdb5ac5e65
KyleKiske <d44i7ryr@yandex.ru>
2023-09-21 14:49:12 +0300

    changed table users name

--- models/users.model.js
+++ models/users.model.js
@@ -1,13 +1,13 @@
 const { db } = require("../config/db.js");
 
 const _register = (email, password) => {
-  return db("signinusers")
+  return db("users")
     .insert({ email, password })
     .returning(["id", "email"]);
 };
 
 const _login = (email) => {
-  return db("signinusers")
+  return db("users")
   .select("id", "email", "password")
   .where({ email });
 };
